810afcd99b7b2ab588536730543ccf514610925e
DylanHP <dylanhp2001@gmail.com>
2026-08-20 12:48:50 +0200

    fix: robust addEventListener (eventi solo se elementi esistono), patcha crash a riga 33

--- assets/js/clippy.js
+++ assets/js/clippy.js
@@ -5,38 +5,36 @@ clippy.load('Clippy', function(agent) {
     agent.play('Greet');
     
     // Interazioni bottoni
-    document.querySelector('.aboutme').addEventListener('click', function() {
+    // Attach listeners SOLO se l'elemento è presente
+    function safeAddSel(sel, ev, fn) {
+        var el = document.querySelector(sel);
+        if (el) el.addEventListener(ev, fn);
+    }
+    safeAddSel('.aboutme', 'click', function() {
         agent.speak("So you want to know more about me? Bold of you to assume there's anything interesting.");
         agent.animate('GetAttention');
     });
-
-    document.querySelector('.projects').addEventListener('click', function() {
+    safeAddSel('.projects', 'click', function() {
         agent.speak("Oh look, more stuff I had to do. Enjoy scrolling through this parade of productivity.");
         agent.animate('GetAttention');
     });
-
-    document.querySelector('.passions').addEventListener('click', function() {
+    safeAddSel('.passions', 'click', function() {
         agent.speak("Apparently I’m supposed to be passionate about these things. Pretend to be impressed.");
         agent.animate('GetAttention');
     });
-
-    document.querySelector('.contacts').addEventListener('click', function() {
+    safeAddSel('.contacts', 'click', function() {
         agent.speak("You want to talk to me? Bold choice. Go ahead, ruin my day.");
         agent.animate('GetAttention');
     });
-
-    document.querySelector('.send').addEventListener('mouseover', function() {
+    safeAddSel('.send', 'mouseover', function() {
         agent.speak("Are you sure you want to send this message? I hope it's not spam!");
         agent.animate('GetAttention');
     });
-
-    document.querySelector('.reset').addEventListener('mouseover', function() {
+    safeAddSel('.reset', 'mouseover', function() {
         agent.speak("I'm glad you want to reset the form! Maybe at the end you di not want to annoy me.");
         agent.animate('GetAttention');
     });
-
-    // Easter egg
-    document.querySelector('.rick').addEventListener('mouseover', function() {
+    safeAddSel('.rick', 'mouseover', function() {
         agent.speak("Maybe there is a secret here.");
         agent.animate('GetAttention');
     });
